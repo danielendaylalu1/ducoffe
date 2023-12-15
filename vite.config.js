65480772fa43026a65f63c1c781b46d532dee3c4
daniel endaylalu <endaylaludaniel@gmail.com>
2023-12-15 10:53:53 +0300

    fixed motion bug

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     external: ["@mui/material/styles"],
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      external: ["@mui/material/styles"],
+    },
+  },
 });
